841f9d9736c013633a6ea4a19335cbb096eeecc8
billelK <bilalkhansal@gmail.com>
2024-08-10 11:56:37 +0200

    get and post tasks

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,28 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig")
+
+async function createTask(payload) {
+    if(!payload.task_completed) {
+        payload.task_completed = false
+    } 
+    const idNewtask = await db("tasks")
+        .insert(payload)
+    const newtask = await db("tasks")
+        .where("task_id", idNewtask).first()
+    if(newtask.task_completed === 0) newtask.task_completed = false
+    else newtask.task_completed = true
+    return newtask
+}
+
+async function findAllTasks() {
+    const result = await db("tasks")
+    result.forEach(project => project.task_completed 
+        ? project.task_completed = true 
+        : project.task_completed = false)
+    return result 
+}
+
+module.exports = {
+    createTask,
+    findAllTasks
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,20 +1,29 @@
 // build your `/api/tasks` router here
 const express = require("express")
+const taskModule = require("./model")
 const router = express.Router()
 
 
-router.get("/", (req,res,next) => {
-    try {
-        res.json("Hello from task router")
-    } catch(error) {
-        next(error)
-    }
+router.post("/", async(req,res,next) => {
+    await taskModule.createTask(req.body)
+        .then(newTask => {
+            res.json(newTask)
+        })
+        .catch(next)
+})
+
+router.get("/", async(req,res,next) => {
+    await taskModule.findAllTasks(req.body)
+        .then(Tasks => {
+            res.json(Tasks)
+        })
+        .catch(next)
 })
 
 router.use((err,req,res,next) => { // eslint-disable-line
     res.status(err.status || 500)
         .json({
-            message: "err.message"
+            message: err.message
         })
 })
 
