f476d4f6fc93db668cc3956d3f7153c52dec706c
billelK <bilalkhansal@gmail.com>
2024-08-10 10:12:52 +0200

    wip project router setup

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
 // build your `/api/projects` router here
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/", (req,res,next) => {
+    res.json("Hello from project router")
+//     try {
+//         res.json("Hello from project router")
+//     } catch(error) {
+//         next(error)
+//     }
+})
+
+router.use((err,req,res,next) => {
+    res.status(err.status || 500)
+        .json({
+            message: "err.message"
+        })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 // build your server here and require it from index.js
 const express = require("express")
+const projectRouter = require("./project/router")
 const server = express()
 
 server.use(express.json())
+server.use("/api/projects", projectRouter)
 
 module.exports = server
